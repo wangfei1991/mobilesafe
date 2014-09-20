de3ae2489a8224852a707d757825ccce6532ac86
wangfei1991 <wangfei199101@gmail.com>
2014-09-20 23:09:32 +0800

    完成了归属地显示

--- MobileSafe/src/com/wyu/mobilesafe/SettingActivity.java
+++ MobileSafe/src/com/wyu/mobilesafe/SettingActivity.java
@@ -3,8 +3,10 @@ package com.wyu.mobilesafe;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.gesture.Prediction;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,7 +14,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import com.wyu.mobilesafe.customeui.SettingItemVIew2;
 import com.wyu.mobilesafe.customeui.SettingItemView;
+import com.wyu.mobilesafe.server.AddressListenerServer;
+import com.wyu.mobilesafe.server.RunningServerUtils;
 import com.wyu.mobilesafe.utils.FontTools;
 
 /**
@@ -25,24 +30,36 @@ public class SettingActivity extends Activity {
 
 	protected static final int SETFONTCODE = 1;
 	private SettingItemView updateItemView;
+	private SettingItemView addressShow;
+	private SettingItemVIew2 addressShowPat;
+	private SettingItemVIew2 settingFont;
 	private TextView settingTitle;
-	private TextView settedFonts_TV;
-	private TextView settingfont_TV;
+//	private TextView settedFonts_TV;
+//	private TextView settingfont_TV;
 	public static String update = "UPDATE";
 	private SharedPreferences pref;
-	private String[] fonts={"stxingka","default","sans","serif","monospace"};
+	private String[] fonts = { "stxingka", "default", "sans", "serif",
+							   "monospace" };
+	private String[] patterns = { "正常", "蓝色", "橙色", "灰色", "绿色" };
 	private int saveFont;
-	
-	private Handler mHandler = new Handler(){
+	private int patternCheckedItem;
+	private Intent addressIntent;
+
+	private Handler mHandler = new Handler() {
 		@Override
 		public void handleMessage(android.os.Message msg) {
 			switch (msg.what) {
 			case SETFONTCODE:
+				FontTools.setFont(SettingActivity.this, settingFont.getItemTitTextView());
+				
+				FontTools.setFont(SettingActivity.this, addressShowPat.getItemTitTextView());
+				FontTools.setFont(SettingActivity.this, addressShowPat.getItemConTextView());
+				
 				FontTools.setFont(SettingActivity.this, settingTitle);
-				FontTools.setFont(SettingActivity.this, settingfont_TV);
 				updateItemView.setFont(settingTitle.getTypeface());
+				addressShow.setFont(settingTitle.getTypeface());
 				int currentFont = pref.getInt("fonts", 0);
-				settedFonts_TV.setText(fonts[currentFont]);
+				settingFont.setItemContext(fonts[currentFont]);
 				saveFont = currentFont;
 				break;
 			default:
@@ -50,24 +67,25 @@ public class SettingActivity extends Activity {
 			}
 		}
 	};
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
 
-		pref = getSharedPreferences("config", MODE_PRIVATE); 
+		pref = getSharedPreferences("config", MODE_PRIVATE);
 		saveFont = pref.getInt("fonts", 0);
-		
-		updateItemView = (SettingItemView) findViewById(R.id.updateSettingItem);
-		settingTitle = (TextView) findViewById(R.id.setting_tv_title);
-		settedFonts_TV = (TextView) findViewById(R.id.settedFonts_TV);
-		settingfont_TV = (TextView) findViewById(R.id.settingfont_TV);
-		
-		FontTools.setFont(this, settingTitle); // 设置字体
-		FontTools.setFont(this, settingfont_TV); // 设置字体
-		settedFonts_TV.setText(fonts[saveFont]);
+		patternCheckedItem = pref.getInt("pattern", 0);
 
+		updateItemView = (SettingItemView) findViewById(R.id.updateSettingItem);
+		addressShow    = (SettingItemView) findViewById(R.id.addressShow);
+		addressShowPat = (SettingItemVIew2) findViewById(R.id.addressShowPattern);
+		settingFont    = (SettingItemVIew2) findViewById(R.id.settingFont);
+		settingTitle   = (TextView) findViewById(R.id.setting_tv_title);
+		
+		settingFont.setItemContext(fonts[saveFont]);
+		
+		
 		/******************** 获取，设置，保存“是否更新”的的状态 **********************/
 		boolean checked = pref.getBoolean(update, true);
 		updateItemView.setChecked(checked);
@@ -86,34 +104,101 @@ public class SettingActivity extends Activity {
 				editor.commit();
 			}
 		});
-		/**************************************************************/		
-	}
-	public void settingFont(View view)
-	{
-		final Editor edit = pref.edit();
-		AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
-		builder.setTitle("设置字体");	
-		
-		builder.setSingleChoiceItems(fonts, saveFont, new AlertDialog.OnClickListener() {			
+		/**************************************************************/
+
+		/********************** 设置监听是否显示来电归属地显示 ****************/
+		boolean isRunning = RunningServerUtils.isRunningServer(
+				SettingActivity.this,
+				"com.wyu.mobilesafe.server.AddressListenerServer");
+		if (isRunning) {
+			addressShow.setChecked(true);
+		} else {
+			addressShow.setChecked(false);
+		}
+		addressIntent = new Intent(SettingActivity.this,
+				AddressListenerServer.class);
+		addressShow.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(DialogInterface dialog, int which) 
-			{				
-				edit.putInt("fonts",which);
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (addressShow.isChecked()) {
+					addressShow.setChecked(false);
+					stopService(addressIntent);
+				} else {
+					addressShow.setChecked(true);
+					startService(addressIntent);
+				}
 			}
 		});
-		builder.setPositiveButton("确定", new AlertDialog.OnClickListener() {
+		/****************************************************************/
+		
+		addressShowPat.setOnClickListener(new OnClickListener() {
 			
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = 
+							new AlertDialog.Builder(SettingActivity.this);	
+				builder.setTitle("样式设置");
+				builder.setSingleChoiceItems(patterns, patternCheckedItem, 
+						new DialogInterface.OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								Editor edit = pref.edit();
+								edit.putInt("pattern", which);
+								edit.commit();
+								addressShowPat.setItemContext(patterns[which]);
+								dialog.dismiss();
+							}
+						});
+				builder.setNegativeButton("取消", null);
+				builder.show();
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		boolean isRunning = RunningServerUtils.isRunningServer(
+				SettingActivity.this,
+				"com.wyu.mobilesafe.server.AddressListenerServer");
+		if (isRunning) {
+			addressShow.setChecked(true);
+		} else {
+			addressShow.setChecked(false);
+		}
+		addressShowPat.setItemContext(patterns[patternCheckedItem]);
+	}
+
+	public void settingFont(View view) {
+		final Editor edit = pref.edit();
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				SettingActivity.this);
+		builder.setTitle("设置字体");
+
+		builder.setSingleChoiceItems(fonts, saveFont,
+				new AlertDialog.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						edit.putInt("fonts", which);
+					}
+				});
+		builder.setPositiveButton("确定", new AlertDialog.OnClickListener() {
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				// TODO Auto-generated method stub
-				edit.commit();	
+				edit.commit();
 				Message msg = new Message();
 				msg.what = SETFONTCODE;
 				mHandler.sendMessage(msg);
 			}
 		});
 		builder.setNegativeButton("取消", new AlertDialog.OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				// TODO Auto-generated method stub
--- MobileSafe/src/com/wyu/mobilesafe/server/AddressListenerServer.java
+++ MobileSafe/src/com/wyu/mobilesafe/server/AddressListenerServer.java
@@ -1,25 +1,38 @@
 package com.wyu.mobilesafe.server;
 
-import com.wyu.mobilesafe.dao.AddressQueryUtils;
-
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
+import android.text.TextUtils;
+
+import com.wyu.mobilesafe.customeui.MyToast;
+import com.wyu.mobilesafe.dao.AddressQueryUtils;
 
 public class AddressListenerServer extends Service {
 
+	protected static final String TAG = "AddressListenerServer";
 	private TelephonyManager manager;
-
+	private OutCallReceiver receiver;
+	private MyToast comingToast;
+	private MyToast outToast;
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
+		
+		
 		manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		manager.listen(phoneStateListener, TelephonyManager.CALL_STATE_RINGING);
+		manager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+		receiver = new OutCallReceiver();
+		IntentFilter filter = new IntentFilter(
+				"android.intent.action.NEW_OUTGOING_CALL");
+		registerReceiver(receiver, filter);
+		
 	}
 
 	@Override
@@ -32,17 +45,54 @@ public class AddressListenerServer extends Service {
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		manager.listen(phoneStateListener, 0);
+		unregisterReceiver(receiver);
 		super.onDestroy();
+	}
+	
+	class OutCallReceiver extends BroadcastReceiver {
+		private String addressPhone;
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			String phone = getResultData();
+			if (!TextUtils.isEmpty(phone)) {
+				addressPhone = AddressQueryUtils.queryAddress(phone);
+				outToast = new MyToast(context, addressPhone);
+				outToast.show();
+			}
+		}
 	}
 
 	private PhoneStateListener phoneStateListener = new PhoneStateListener() {
 		public void onCallStateChanged(int state, String incomingNumber) {
-			if (state == TelephonyManager.CALL_STATE_RINGING) {
+			super.onCallStateChanged(state, incomingNumber);
+			
+			
+			switch (state) {
+			case TelephonyManager.CALL_STATE_RINGING:
 				String queryAddress = AddressQueryUtils
 						.queryAddress(incomingNumber);
-				Toast.makeText(getBaseContext(), queryAddress, 1).show();
+				comingToast = new MyToast(getApplicationContext(), queryAddress);
+				comingToast.show();
+				break;
+			case TelephonyManager.CALL_STATE_IDLE:
+				if(comingToast != null){
+					comingToast.dismiss();
+				}
+				comingToast = null;
+				if (outToast != null) {
+					outToast.dismiss();
+				}
+				outToast = null;
+				break;
+				
+			default:
+				break;
 			}
+
 		}
 	};
+
+	
 
 }
